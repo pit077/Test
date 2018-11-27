8ea7058c903a3ffcc3e898c6f5517ff779352674
pit077 <pit077@mail.ru>
2018-11-27 19:58:36 +0300

    Добавил еще 3 класса определяющие,являются ли юниты атакующими,или защитниками BasicCavalry; BasicMagistrate; BasicBestiary

---
+++ Servers/Армия/Бестиария/BasicBestiary.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/// <summary>
+/// Класс определяет,является юнит атакующим,или защитником для Бестиарии
+/// </summary>
+
+namespace Servers
+{
+    public abstract class BasicBestiary : BasicWarrior
+    {
+        public BasicBestiary(ConstructionTime constructionTime) : base(constructionTime) { }
+
+        #region Грифон
+        public class Griffin : BasicBestiary
+        {
+            public override bool off { get; set; } = false;
+            public Griffin(ConstructionTime constructionTime) : base(constructionTime) { }
+        }
+        #endregion
+        #region Виверна
+        public class Wyvern : BasicBestiary
+        {
+            public override bool off { get; set; } = true;
+            public Wyvern(ConstructionTime constructionTime) : base(constructionTime) { }
+        }
+        #endregion
+        #region Дракон
+        public class Dragon : BasicBestiary
+        {
+            public override bool off { get; set; } = true;
+            public Dragon(ConstructionTime constructionTime) : base(constructionTime) { }
+        }
+        #endregion
+        #region Химера
+        public class Chimera : BasicBestiary
+        {
+            public override bool off { get; set; } = true;
+            public Chimera(ConstructionTime constructionTime) : base(constructionTime) { }
+        }
+        #endregion
+        #region Пожиратель
+        public class Devourer : BasicBestiary
+        {
+            public override bool off { get; set; } = false;
+            public Devourer(ConstructionTime constructionTime) : base(constructionTime) { }
+        }
+        #endregion
+        #region Шпион
+        public class Spy : BasicBestiary
+        {
+            public override bool off { get; set; } = false;
+            public Spy(ConstructionTime constructionTime) : base(constructionTime) { }
+        }
+        #endregion
+        #region Суккуб
+        public class Sukkub : BasicBestiary
+        {
+            public override bool off { get; set; } = false;
+            public Sukkub(ConstructionTime constructionTime) : base(constructionTime) { }
+        }
+        #endregion
+        #region Ассасин
+        public class Assasin : BasicBestiary
+        {
+            public override bool off { get; set; } = false;
+            public Assasin(ConstructionTime constructionTime) : base(constructionTime) { }
+        }
+        #endregion
+        #region Мародер
+        public class Marauder : BasicBestiary
+        {
+            public override bool off { get; set; } = true;
+            public Marauder(ConstructionTime constructionTime) : base(constructionTime) { }
+        }
+        #endregion
+        #region Имперский грифон
+        public class ImperialGriffin : BasicBestiary
+        {
+            public override bool off { get; set; } = false;
+            public ImperialGriffin(ConstructionTime constructionTime) : base(constructionTime) { }
+        }
+        #endregion
+        #region Имперская виверна
+        public class ImperialWyvern : BasicBestiary
+        {
+            public override bool off { get; set; } = true;
+            public ImperialWyvern(ConstructionTime constructionTime) : base(constructionTime) { }
+        }
+        #endregion
+        #region Имперский дракон
+        public class ImperialDragon : BasicBestiary
+        {
+            public override bool off { get; set; } = true;
+            public ImperialDragon(ConstructionTime constructionTime) : base(constructionTime) { }
+        }
+        #endregion
+        #region Имперская химера
+        public class ImperialChimera : BasicBestiary
+        {
+            public override bool off { get; set; } = true;
+            public ImperialChimera(ConstructionTime constructionTime) : base(constructionTime) { }
+        }
+        #endregion
+        #region Имперский Пожиратель
+        public class ImperialDevourer : BasicBestiary
+        {
+            public override bool off { get; set; } = false;
+            public ImperialDevourer(ConstructionTime constructionTime) : base(constructionTime) { }
+        }
+        #endregion
+        #region Имперский шпион
+        public class ImperialSpy : BasicBestiary
+        {
+            public override bool off { get; set; } = false;
+            public ImperialSpy(ConstructionTime constructionTime) : base(constructionTime) { }
+        }
+        #endregion
+        #region Имперский суккуб
+        public class ImperialSukkub : BasicBestiary
+        {
+            public override bool off { get; set; } = false;
+            public ImperialSukkub(ConstructionTime constructionTime) : base(constructionTime) { }
+        }
+        #endregion
+        #region Имперский ассасин
+        public class ImperialAssasin : BasicBestiary
+        {
+            public override bool off { get; set; } = false;
+            public ImperialAssasin(ConstructionTime constructionTime) : base(constructionTime) { }
+        }
+        #endregion
+        #region Темный Грифон
+        public class DarkGriffin : BasicBestiary
+        {
+            public override bool off { get; set; } = false;
+            public DarkGriffin(ConstructionTime constructionTime) : base(constructionTime) { }
+        }
+        #endregion
+        #region Темная Виверна
+        public class DarkWyvern : BasicBestiary
+        {
+            public override bool off { get; set; } = true;
+            public DarkWyvern(ConstructionTime constructionTime) : base(constructionTime) { }
+        }
+        #endregion
+        #region Темный Дракон
+        public class DarkDragon : BasicBestiary
+        {
+            public override bool off { get; set; } = true;
+            public DarkDragon(ConstructionTime constructionTime) : base(constructionTime) { }
+        }
+        #endregion
+        #region Темная Химера
+        public class DarkChimera : BasicBestiary
+        {
+            public override bool off { get; set; } = true;
+            public DarkChimera(ConstructionTime constructionTime) : base(constructionTime) { }
+        }
+        #endregion
+        #region Темный Пожиратель
+        public class DarkDevourer : BasicBestiary
+        {
+            public override bool off { get; set; } = false;
+            public DarkDevourer(ConstructionTime constructionTime) : base(constructionTime) { }
+        }
+        #endregion
+        #region Темный Шпион
+        public class DarkSpy : BasicBestiary
+        {
+            public override bool off { get; set; } = false;
+            public DarkSpy(ConstructionTime constructionTime) : base(constructionTime) { }
+        }
+        #endregion
+        #region Темный Суккуб
+        public class DarSukkub : BasicBestiary
+        {
+            public override bool off { get; set; } = false;
+            public DarSukkub(ConstructionTime constructionTime) : base(constructionTime) { }
+        }
+        #endregion
+        #region Темный Ассасин
+        public class DarAssasin : BasicBestiary
+        {
+            public override bool off { get; set; } = false;
+            public DarAssasin(ConstructionTime constructionTime) : base(constructionTime) { }
+        }
+        #endregion
+        #region Возрожденный Грифон
+        public class RevivedGriffin : BasicBestiary
+        {
+            public override bool off { get; set; } = false;
+            public RevivedGriffin(ConstructionTime constructionTime) : base(constructionTime) { }
+        }
+        #endregion
+        #region Возрожденный Дракон
+        public class RevivedDragon : BasicBestiary
+        {
+            public override bool off { get; set; } = true;
+            public RevivedDragon(ConstructionTime constructionTime) : base(constructionTime) { }
+        }
+        #endregion
+        #region Кошмар
+        public class Nightmare : BasicBestiary
+        {
+            public override bool off { get; set; } = false;
+            public Nightmare(ConstructionTime constructionTime) : base(constructionTime) { }
+        }
+        #endregion
+        #region Инферно
+        public class Inferno : BasicBestiary
+        {
+            public override bool off { get; set; } = true;
+            public Inferno(ConstructionTime constructionTime) : base(constructionTime) { }
+        }
+        #endregion
+        #region Архангел
+        public class Archangel : BasicBestiary
+        {
+            public override bool off { get; set; } = false;
+            public Archangel(ConstructionTime constructionTime) : base(constructionTime) { }
+        }
+        #endregion
+        #region Потрошитель
+        public class Eviscerator : BasicBestiary
+        {
+            public override bool off { get; set; } = false;
+            public Eviscerator(ConstructionTime constructionTime) : base(constructionTime) { }
+        }
+        #endregion
+    }
+}
---
+++ Servers/Армия/Магистрат/BasicMagistrate.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/// <summary>
+/// Класс определяет,является юнит атакующим,или защитником для Магистрата
+/// </summary>
+
+namespace Servers
+{
+    public abstract class BasicMagistrate : BasicWarrior
+    {
+        public BasicMagistrate(ConstructionTime constructionTime) : base(constructionTime) { }
+
+        #region Крушитель
+        public class Krushitel : BasicMagistrate
+        {
+            public override bool off { get; set; } = false;
+            public Krushitel(ConstructionTime constructionTime) : base(constructionTime) { }
+        }
+        #endregion
+        #region Голем
+        public class Golem : BasicMagistrate
+        {
+            public override bool off { get; set; } = false;
+            public Golem(ConstructionTime constructionTime) : base(constructionTime) { }
+        }
+        #endregion
+        #region Чернокнижник
+        public class Magician : BasicMagistrate
+        {
+            public override bool off { get; set; } = true;
+            public Magician(ConstructionTime constructionTime) : base(constructionTime) { }
+        }
+        #endregion
+        #region Демон
+        public class Demon : BasicMagistrate
+        {
+            public override bool off { get; set; } = false;
+            public Demon(ConstructionTime constructionTime) : base(constructionTime) { }
+        }
+        #endregion
+        #region Некромант
+        public class Necromancer : BasicMagistrate
+        {
+            public override bool off { get; set; } = true;
+            public Necromancer(ConstructionTime constructionTime) : base(constructionTime) { }
+        }
+        #endregion
+        #region Ренегат
+        public class Renegade : BasicMagistrate
+        {
+            public override bool off { get; set; } = true;
+            public Renegade(ConstructionTime constructionTime) : base(constructionTime) { }
+        }
+        #endregion
+        #region Имперский голем
+        public class ImperialGolem : BasicMagistrate
+        {
+            public override bool off { get; set; } = false;
+            public ImperialGolem(ConstructionTime constructionTime) : base(constructionTime) { }
+        }
+        #endregion
+        #region Имперский чернокнижник
+        public class ImperialMagician : BasicMagistrate
+        {
+            public override bool off { get; set; } = true;
+            public ImperialMagician(ConstructionTime constructionTime) : base(constructionTime) { }
+        }
+        #endregion
+        #region Имперский демон
+        public class ImperialDemon : BasicMagistrate
+        {
+            public override bool off { get; set; } = false;
+            public ImperialDemon(ConstructionTime constructionTime) : base(constructionTime) { }
+        }
+        #endregion
+        #region Имперский некромант
+        public class ImperialNecromancer : BasicMagistrate
+        {
+            public override bool off { get; set; } = true;
+            public ImperialNecromancer(ConstructionTime constructionTime) : base(constructionTime) { }
+        }
+        #endregion
+        #region Имперский ренегат
+        public class ImperialRenegade : BasicMagistrate
+        {
+            public override bool off { get; set; } = true;
+
+            public ImperialRenegade(ConstructionTime constructionTime) : base(constructionTime) { }            
+        }
+        #endregion
+        #region Темный Голем
+        public class DarkGolem : BasicMagistrate
+        {
+            public override bool off { get; set; } = false;
+            public DarkGolem(ConstructionTime constructionTime) : base(constructionTime) { }
+        }
+        #endregion
+        #region Темный Чернокнижник
+        public class DarkMagician : BasicMagistrate
+        {
+            public override bool off { get; set; } = true;
+            public DarkMagician(ConstructionTime constructionTime) : base(constructionTime) { }
+        }
+        #endregion
+        #region Темный Демон
+        public class DarkDemon : BasicMagistrate
+        {
+            public override bool off { get; set; } = false;
+            public DarkDemon(ConstructionTime constructionTime) : base(constructionTime) { }
+        }
+        #endregion
+        #region Темный Некромант
+        public class DarkNecromancer : BasicMagistrate
+        {
+            public override bool off { get; set; } = true;
+            public DarkNecromancer(ConstructionTime constructionTime) : base(constructionTime) { }
+        }
+        #endregion
+        #region Возрожденный Голем
+        public class RevivedGolem : BasicMagistrate
+        {
+            public override bool off { get; set; } = false;
+            public RevivedGolem(ConstructionTime constructionTime) : base(constructionTime) { }
+        }
+        #endregion
+        #region Возрожденный Некромант
+        public class RevivedNecromancer : BasicMagistrate
+        {
+            public override bool off { get; set; } = true;
+            public RevivedNecromancer(ConstructionTime constructionTime) : base(constructionTime) { }
+        }
+        #endregion
+        #region Людоед
+        public class Cannibal : BasicMagistrate
+        {
+            public override bool off { get; set; } = false;
+            public Cannibal(ConstructionTime constructionTime) : base(constructionTime) { }
+        }
+        #endregion
+        #region Падший
+        public class Fallen : BasicMagistrate
+        {
+            public override bool off { get; set; } = true;
+            public Fallen(ConstructionTime constructionTime) : base(constructionTime) { }
+        }
+        #endregion
+        #region Гидра
+        public class Hydra : BasicMagistrate
+        {
+            public override bool off { get; set; } = false;
+            public Hydra(ConstructionTime constructionTime) : base(constructionTime) { }
+        }
+        #endregion
+    }
+}
